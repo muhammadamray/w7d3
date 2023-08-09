945f6a8221e76738331210ad17d06800745f7d38
Muhammad Amray <muhammadamray@gmail.com>
2023-08-09 15:02:43 -0400

    fixed and made specs run

---
+++ practice-for-ch-rails-goal-app-long-practice-main/goals_app/spec/models/user_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  subject { User.create!(username: 'Alex', password: 'password') }
+
+  it { should validate_presence_of(:username) }
+  it { should validate_uniqueness_of(:username) }
+  it { should validate_uniqueness_of(:session_token) }
+  it { should validate_presence_of(:password_digest) }
+  it { should validate_length_of(:password).is_at_least(6) }
+
+
+
+
+
+end
